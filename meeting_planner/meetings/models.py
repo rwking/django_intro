ce88fdbb3caec9b985ab7856045dc8c192774077
Rich King <rking@remedypartners.com>
2021-04-16 18:46:21 -0400

    updated gitignores and models

--- meeting_planner/meetings/models.py
+++ meeting_planner/meetings/models.py
@@ -1,3 +1,13 @@
+from datetime import time
+
 from django.db import models
 
 # Create your models here.
+class Meeting(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.title} at {self.start_time} on {self.date}"
