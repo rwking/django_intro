8781f393ff90935db949b17de2b6aa6796a553d8
Rich King <rwking@gmail.com>
2021-04-18 20:52:01 -0400

    final commit for django intro course

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.forms import modelform_factory
 
 from .models import Meeting, Room
 
@@ -12,3 +13,17 @@ def detail(request, id):
 def rooms_list(request):
     return render(request, "meetings/rooms.html",
         {"rooms": Room.objects.all()})
+
+
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
+
+def new(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
+    return render(request, 'meetings/new.html', {"form": form})
